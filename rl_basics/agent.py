e5f38a8c58a796c70b479dfd3ca677d73ad0d29f
rmathew-uni <rmathew@uni-osnabrueck.de>
2026-05-25 15:08:10 +0000

    added basic rl agent and reward.
    TODO write run_experiment code.

--- rl_basics/agent.py
+++ rl_basics/agent.py
@@ -1,10 +1,10 @@
-#%%
+# %%
 import numpy as np
 from copy import deepcopy
 
 
 # %%
-class agent():
+class agent:
     def __init__(self, gamma, reward_function, env, learning_rate, beta):
         self.gamma = gamma
         self.reward_function = reward_function
@@ -14,17 +14,21 @@ class agent():
         self.state = env.__init__()
         self.V = np.zeros((env.room_size, env.room_size))
         self.action_space = env.action_space()
-    
+
     def update_V(self, s, snext):
         r = self.reward_function(self.env, s)
         V = self.V
-        V[s[0]][s[1]] = V[s[0]][s[1]] + self.lr * (r+-V[s[0]][s[1]])
+        V[s[0]][s[1]] = V[s[0]][s[1]] + self.lr * (
+            r + self.gamma * self.V[snext[0]][snext[1]] - V[s[0]][s[1]]
+        )
         self.V = V
         return r
+
     def softmax_choice(self, Qs):
         ps = np.exp(self.beta * Qs) / np.sum(np.exp(self.beta * Qs))
         a = np.random.choice(len(ps), p=ps)
         return a
+
     def choose_action(self):
         Qs = np.zeros(len(self.action_space))
         env = self.env
@@ -35,6 +39,13 @@ class agent():
         return a
 
 
-class reward_function(env, position,):
-    
+# %%
+def reward_function(
+    env, position
+):  # Return 1 if agent is at goal position or 0 otherwiser
+    if env.agent_position == position:
+        return 1
+    else:
+        return 0
 
+# %%
---
+++ rl_basics/plots.py
@@ -0,0 +1,12 @@
+from matplotlib.pyplot as plt
+
+def plot_rewards():
+    pass
+
+def plot_q_value_map():
+    pass
+
+def plot_step_wise_q_value():
+    pass
+
+
